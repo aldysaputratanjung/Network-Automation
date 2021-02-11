4a8c516fdda3278849a05df1c7bb076ec2aa4776
aldysaputratanjung <aldysaputratanjung@gmail.com>
2021-02-11 12:41:06 +0700

    Update

--- netmiko/netmiko3.py
+++ netmiko/netmiko3.py
@@ -2,22 +2,22 @@ from netmiko import ConnectHandler
 
 iosv_l2_s4 = {
     'device_type': 'cisco_ios',
-    'ip': '192.168.122.84',
-    'username': 'david',
+    'ip': '192.168.122.149',
+    'username': 'aldi',
     'password': 'cisco',
 }
 
 iosv_l2_s5 = {
     'device_type': 'cisco_ios',
-    'ip': '192.168.122.85',
-    'username': 'david',
+    'ip': '192.168.122.150',
+    'username': 'aldi',
     'password': 'cisco',
 }
 
 iosv_l2_s6 = {
     'device_type': 'cisco_ios',
-    'ip': '192.168.122.86',
-    'username': 'david',
+    'ip': '192.168.122.151',
+    'username': 'aldi',
     'password': 'cisco',
 }
 
